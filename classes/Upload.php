5e2aab35ba87b772c9111726216900c795b9e816
mickael-2001dev <mickael.souza.if@gmail.com>
2017-05-06 00:48:44 -0300

    Upload

--- classes/Upload.php
+++ classes/Upload.php
@@ -1,40 +1,50 @@
 <?php
 /*
-    ---01/05/2017
+    ---06/05/2017
 
     Criado Por Mickael Braz de Souza
     Sistema de Uploads de imagem com corte e
     redimensionamento com utilizando o plugin 
     para jquery, Jcrop e a classe para
-    PHP, WideImage.
-
-    ----01/05/2017---
+    PHP, WideImage. Algumas alterações foram
+    feitas para redução de código, e também,
+    temos algumas mudanças na nomenclatura
+    dos atributos e métodos.
+    
+    ----06/05/2017---
 */
 class Upload {
-    private $arquivo;
-    private $nome;
-    private $extn;
-    private $novo;
+    private $file;
+    private $name;
+    private $exten;
+    private $newName;
     private $result;
     private $msg;
-    private $diretorio;
-    public $dir_img;
+    private $folder;
+    private $dir_img;
     
-    public function __construct() {
-        $this->diretorio = "img/";
+    public function __construct(array $img) {
+        $this->folder = "img/";
+        //Metódo responsável por criar o diretório onde a imagem será salva
+        $this->name = $img['name'];//Atribuindo o nome
+        $this->exten = @end(explode('.', $this->name));//Obtendo a extensão
+        $this->file = $img;//Atribuindo o arquivo
+        $this->newName = rand().'.'.$this->extn;//Criando um novo nome aleatório
+        $this->dir_img = $this->folder.''.$this->newName;//Criando um novo Diretório
+        $this->moveFile();//Chamando o metódo moveArquivo, responsável por salvar a imagem
     }
-    public function image(array $img){//Metódo responsável por criar o diretório onde a imagem será salva
+    /*public function image(){//Metódo responsável por criar o diretório onde a imagem será salva
         $this->nome= $img['name'];//Atribuindo o nome
         $this->extn = @end(explode('.', $this->nome));//Obtendo a extensão
         $this->arquivo = $img;//Atribuindo o arquivo
         $this->novo = rand().'.'.$this->extn;//Criando um novo nome aleatório
         $this->dir_img = $this->diretorio.''.$this->novo;//Criando um novo Diretório
         $this->moveArquivo();//Chamando o metódo moveArquivo, responsável por salvar a imagem
-    }
-    private function moveArquivo() {
+    }*/
+    private function moveFile() {
 
-        if(move_uploaded_file($this->arquivo['tmp_name'], $this->dir_img)){//Salvando a Imagem no diretório
-            $this->result = $this->nome;//Atribuindo o nome ao resultado, para o atributo não ficar vazio
+        if(move_uploaded_file($this->file['tmp_name'], $this->dir_img)){//Salvando a Imagem no diretório
+            $this->result = true;//Atribuindo true para o resultado, caso seja possivel realiszar o upload
             $this->msg = 'Upload realizado com sucesso';
         }else{            
             $this->result = false;//Atribuindo false ao resultado, caso não seja possivel, fazer o upload
@@ -48,11 +58,5 @@ class Upload {
 
     public function getMsg() {
         return $this->msg;
-    }
-    public function getNovo(){
-        return $this->novo;
-    }
-    public function getDiretorio(){
-        return $this->diretorio;
     }
 }
